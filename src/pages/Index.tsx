e149ea6ee95ae911d2e05a84397849e4e733236f
Manaskannikanti <149857102+Manaskannikanti@users.noreply.github.com>
2026-01-28 14:46:27 +0530

    Update Index.tsx

--- src/pages/Index.tsx
+++ src/pages/Index.tsx
@@ -2,37 +2,21 @@ import { Navbar } from "@/components/landing/Navbar";
 import { HeroSection } from "@/components/landing/HeroSection";
 import { FeaturesSection } from "@/components/landing/FeaturesSection";
 import { HowItWorksSection } from "@/components/landing/HowItWorksSection";
+import { PricingSection } from "@/components/landing/PricingSection";
+import { AboutSection } from "@/components/landing/AboutSection";
 import { CTASection } from "@/components/landing/CTASection";
 import { Footer } from "@/components/landing/Footer";
-import { AboutSection } from "@/components/landing/AboutSection";
-import { PricingSection } from "@/components/landing/PricingSection";
 
 const Index = () => {
   return (
     <div className="min-h-screen">
       <Navbar />
       <main className="pt-16">
-        <section id="home">
-          <HeroSection />
-        </section>
-
-        <section id="features">
-          <FeaturesSection />
-        </section>
-
-        <section id="how-it-works">
-          <HowItWorksSection />
-          
-        </section>
-
-        <section id="about">
-          <AboutSection />
-        </section>
-
-        <section id="pricing">
-          <PricingSection />
-        </section>
-
+        <HeroSection />
+        <FeaturesSection />
+        <HowItWorksSection />
+        <PricingSection />
+        <AboutSection />
         <CTASection />
       </main>
       <Footer />
